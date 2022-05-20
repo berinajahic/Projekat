32fab08b781b4b21930e18dba62463c8ba83f0c7
berinajahic <96484622+berinajahic@users.noreply.github.com>
2022-05-20 14:24:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,72 @@
-fetch('https://ptf-web-dizajn-2022.azurewebsites.net/books')
+fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Services')
   .then(response => {
     return response.json();
   })
   .then(data => {
-    renderBooks(data);
+    renderService(data);
   })
 
-const renderBooks = (books) => {
-    const bookRow = document.getElementById('books-row');
-    let resultHtml = '';
+const renderService = (services) => {
+  const servicesRow = document.getElementById('service-row');
+  let resultHtml = '';
 
-    books.forEach(book => {
-      resultHtml += `
+  services.forEach(service => {
+    resultHtml += `
        <div class="card col-4 mx-2 my-2" style="width: 18rem;">
-            <img src= "${book.image}" class="card-img-top" alt="Books">
+            <img src= "${service.photoUrl}" class="card-img-top" alt="service">
              <div class="card-body">
-                 <h5 class="card-title">${book.name}</h5>
+                 <h5 class="card-title">${service.name}</h5>
                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                 <button type="button" class="btn btn-primary" onclick="completeTodo(${book.id})">Complete</button>
-                 <button type="button" class="btn btn-danger" onclick="deleteTodo(${book.id})">Delete</button>
-                 <span class="badge bg-${book.isCompleted ? 'success' : 'danger'}">${book.isCompleted ? 'Completed' : 'Active'}</span>
+                 <button type="button" class="btn btn-primary" onclick="completeService(${service.id})">Complete</button>
+                 <button type="button" class="btn btn-danger" onclick="deleteService(${service.id})">Delete</button>
+                 <span class="badge bg-${service.isCompleted ? 'Done' : 'Not done'}">${service.isCompleted ? 'Completed' : 'Not completed'}</span>
              </div>
          </div>`;
-         console.log(book)
-    });
+  });
 
-         bookRow.innerHTML = resultHtml;
-    }
-    //
-    // const completeTodo = (id) => {
-    //     fetch(`${BASE_URL}/api/todos/${id}/complete`, {
-    //         method: 'PUT'
-    //     })
-    //     .then(res => {
-    //         console.log(res);
-    //     })
-    // }
-    //
-    // const addTodo = () => {
-    //     const todoName = document.getElementById('todo-name').value;
-    //
-    //     fetch(`${BASE_URL}/api/todos/new`, {
-    //         method: 'POST',
-    //         headers: new Headers({'content-type': 'application/json'}),
-    //         body: JSON.stringify({
-    //             name: todoName
-    //         })
-    //     })
-    //     .then(res => {
-    //         console.log(res);
-    //     })
-    // }
-    //
-    // const deleteTodo = (id) => {
-    //     fetch(`${BASE_URL}/api/todos/${id}`, {
-    //         method: 'DELETE'
-    //     })
-    //     .then(res => {
-    //         console.log(res);
-    //     })
-    // }console.log('tekst')
+  servicesRow.innerHTML = resultHtml;
+}
+//Put updejta resurs na bekendu
+// const completeBook = () => {
+//   fetch('https://ptf-web-dizajn-2022.azurewebsites.net/books', {
+//       method: 'PUT'
+//     })
+//     .then(res => {
+//       console.log(res);
+//     })
+// }
+
+const addService = () => {
+   const serviceID = document.getElementById('service-id').value;
+   const serviceName = document.getElementById('service-name').value;
+   const servicePrice = document.getElementById('service-price').value;
+   const serviceImage = document.getElementById('service-image').value;
+
+   fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Services`, {
+       method: 'POST',
+       headers: new Headers({
+         'content-type': 'application/json'
+       }),
+       body: JSON.stringify({
+
+         id: serviceID,
+         name: serviceName,
+         price: servicePrice,
+         image: serviceImage,
+
+       })
+   })
+     .then(res => {
+       console.log(res);
+     })
+
+ }
+
+  const deleteService = (id) => {
+      fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Services/${id}`, {
+         method: 'DELETE'
+     })
+      .then(res => {
+          console.log(res);
+      })
+  }
