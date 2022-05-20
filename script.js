d6aa6f9c2909689fb36dd8c788de02c80ee4bc76
berinajahic <96484622+berinajahic@users.noreply.github.com>
2022-05-21 00:35:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,13 +13,11 @@ const renderService = (services) => {
   services.forEach(service => {
     resultHtml += `
        <div class="card col-4 mx-2 my-2" style="width: 18rem;">
-            <img src= "${service.photoUrl}" class="card-img-top" alt="service">
+            <img class="imgCard" src= "${service.photoUrl}" class="card-img-top" alt="service">
              <div class="card-body">
                  <h5 class="card-title">${service.name}</h5>
-                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                 <button type="button" class="btn btn-primary" onclick="completeService(${service.id})">Complete</button>
-                 <button type="button" class="btn btn-danger" onclick="deleteService(${service.id})">Delete</button>
-                 <span class="badge bg-${service.isCompleted ? 'Done' : 'Not done'}">${service.isCompleted ? 'Completed' : 'Not completed'}</span>
+                 <button type="button" class="btn delete" onclick="deleteService(${service.id})">Delete</button>
+                 <button type="button" class="btn edit" onclick="editServices()">Edit</button>
              </div>
          </div>`;
   });
@@ -27,46 +25,59 @@ const renderService = (services) => {
   servicesRow.innerHTML = resultHtml;
 }
 //Put updejta resurs na bekendu
-// const completeBook = () => {
-//   fetch('https://ptf-web-dizajn-2022.azurewebsites.net/books', {
-//       method: 'PUT'
-//     })
-//     .then(res => {
-//       console.log(res);
-//     })
-// }
+const editServices = () => {
+  const service_ID = document.getElementById('service-i').value;
+  const service_Name = document.getElementById('service-n').value;
+  const service_Price = document.getElementById('service-p').value;
+  const service_Image = document.getElementById('service-i').value;
+  fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Services', {
+      method: 'PUT',
+      headers: new Headers({
+        'content-type': 'application/json'
+      }),
+      body: JSON.stringify({
+        id: service_ID,
+        name: service_Name,
+        price: service_Price,
+        photoUrl: service_Image
+      })
+    })
+    .then(res => {
+      console.log(res);
+    })
+}
 
 const addService = () => {
-   const serviceID = document.getElementById('service-id').value;
-   const serviceName = document.getElementById('service-name').value;
-   const servicePrice = document.getElementById('service-price').value;
-   const serviceImage = document.getElementById('service-image').value;
+  const serviceID = document.getElementById('service-id').value;
+  const serviceName = document.getElementById('service-name').value;
+  const servicePrice = document.getElementById('service-price').value;
+  const serviceImage = document.getElementById('service-image').value;
 
-   fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Services`, {
-       method: 'POST',
-       headers: new Headers({
-         'content-type': 'application/json'
-       }),
-       body: JSON.stringify({
+  fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Services`, {
+      method: 'POST',
+      headers: new Headers({
+        'content-type': 'application/json'
+      }),
+      body: JSON.stringify({
 
-         id: serviceID,
-         name: serviceName,
-         price: servicePrice,
-         image: serviceImage,
+        id: serviceID,
+        name: serviceName,
+        price: servicePrice,
+        photoUrl: serviceImage
 
-       })
-   })
-     .then(res => {
-       console.log(res);
-     })
-
- }
-
-  const deleteService = (id) => {
-      fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Services/${id}`, {
-         method: 'DELETE'
-     })
-      .then(res => {
-          console.log(res);
       })
-  }
+    })
+    .then(res => {
+      console.log(res);
+    })
+
+}
+
+const deleteService = (id) => {
+  fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Services/${id}`, {
+      method: 'DELETE'
+    })
+    .then(res => {
+      console.log(res);
+    })
+}
